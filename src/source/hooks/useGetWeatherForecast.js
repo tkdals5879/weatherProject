3ee0eb63821256a89f64a17c47826e69dd7e6ce0
tkdals5879 <tkdals58799@gmail.com>
2025-02-27 11:42:30 +0900

    forecast의 주소를 http에서 https로 변경

--- src/source/hooks/useGetWeatherForecast.js
+++ src/source/hooks/useGetWeatherForecast.js
@@ -5,7 +5,7 @@ const myWeatherApiKey = process.env.REACT_APP_WEATHER_API_KEY
 
 const fetchWeatherForecast = async({queryKey}) => {
   const cityName = queryKey[1]
-  const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${myWeatherApiKey}`)
+  const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${myWeatherApiKey}`)
   // api받아올 때 한국 기준으로 받아오니까 이 값을 동적으로 바꿔야함
   return response.data;
 }
